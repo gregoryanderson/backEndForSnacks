824ea27b0f5937083d51f88d3c6c72aa90c6764a
Gregory Anderson <anderson.gregory.mail@gmail.com>
2019-10-07 09:55:16 -0600

    Reassign Port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3001);
 app.locals.title = "Connections";
 
 app.get("/", (request, response) => {
