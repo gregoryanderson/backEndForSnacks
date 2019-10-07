70ff27ecf41a0d87b8541c1e911c3a6ab97b36ff
Gregory Anderson <anderson.gregory.mail@gmail.com>
2019-10-07 11:55:20 -0600

    Add procfile

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 app.set("port", process.env.PORT || 3001);
 app.locals.title = "Connections";
+app.locals.snacks = [
+  { id: 1, name: "Almonds", delicious: true },
+  { id: 2, name: "Avocado", delicious: true },
+  { id: 3, name: "Pizza", delicious: true },
+  { id: 4, name: "Tacos", delicious: true }
+]
 
-app.get("/", (request, response) => {
+app.get("/", cors(), (request, response) => {
   response.send("here is some connections");
+});
+
+app.get("/api/v1/snacks", (request, response) => {
+  const { snacks } = app.locals;
+  return response.status(200).json( snacks );
 });
 
 app.listen(app.get("port"), () => {
